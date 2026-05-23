1c0c97a2cc151531f6914f6db22febe1f8b5dbf4
bilalparquet-spec <bilalparquet@gmail.com>
2026-05-23 01:32:22 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import "./globals.css";
+import App from "./DjazairApp";
+
+export default function Page() {
+  return (
+    <div style={{ height: "100vh", overflow: "hidden" }}>
+      <App />
+    </div>
+  );
+}
